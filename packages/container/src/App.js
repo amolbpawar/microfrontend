6dfd96c8db6d86b6dfefb847b427d1a0a5e13acf
Amol <amolpawar2018@outlook.com>
2021-05-15 20:21:23 +0530

    Initial Commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MarketingApp from './components/MarketingApp';
+
+export default () => {
+  return (
+    <div>
+      <h1>Hellli I am here</h1>
+      <hr />
+      <MarketingApp />
+    </div>
+  )
+}
